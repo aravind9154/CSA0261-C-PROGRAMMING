ad8d0f9e1a79058bc256dd6677a83a20c76eef19
aravind9154 <114474802+aravind9154@users.noreply.github.com>
2023-03-31 22:40:59 +0530

    Add files via upload

---
+++ daily practice 1/5.c(day 4).cpp
@@ -0,0 +1,16 @@
+ #include<stdio.h>
+ struct area
+ {
+ 	float length;
+ 	flaot width;
+ }a;
+ main()
+ {
+ 	int area;
+ 	printf("enter the length:");
+ 	scanf("%f",&a,length);
+ 	printf("enter the width:");
+ 	scanf("%f",&a,width);
+ 	area=a.width*a.length;
+ 	printf("%f",area); 	
+}
